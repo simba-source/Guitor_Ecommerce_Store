82a9fc8d29c2939be3814c8d9781223898c38f6a
mperrott <mperrott@uvm.edu>
2022-11-08 16:38:45 -0500

    Added USER and PURCHASE functionality and data

--- SQL/start_data.sql
+++ SQL/start_data.sql
@@ -1,17 +1,12 @@
--- create sample user (10-digit id)
 INSERT INTO USER VALUES (0123456789, 'Sample', 'User', 'Sample-User-205', 'software', 100.00);
-
--- create companies (5-digit id)
 INSERT INTO COMPANY VALUES (47350, 'Gibson');
 INSERT INTO COMPANY VALUES (36613, 'Fender');
 INSERT INTO COMPANY VALUES (90346, 'Taylor');
 INSERT INTO COMPANY VALUES (80816, 'Martin');
 INSERT INTO COMPANY VALUES (81080, 'Paul Reed Smith');
 INSERT INTO COMPANY VALUES (96852, 'Mitchell');
-
--- create guitars (7-digit id)
 INSERT INTO GUITAR VALUES (2099551, 'Gibson SG Standard Electric Guitar', 1799.99, 47350);
-INSERT INTO GUITAR VALUES (5495151, 'Gibson Les Paul Standard \'60s Electric Guitar', 2799.99, 47350);
+INSERT INTO GUITAR VALUES (5495151, 'Gibson Les Paul Standard 60s Electric Guitar', 2799.99, 47350);
 INSERT INTO GUITAR VALUES (9498421, 'Gibson ES-335 Semi-Hollow Electric Guitar', 3499.99, 47350);
 INSERT INTO GUITAR VALUES (3836594, 'Gibson F-25 Folksinger 1965 Acoustic Guitar', 2699.99, 47350);
 INSERT INTO GUITAR VALUES (4946939, 'Fender Player Series Stratocaster Maple Fingerboard Electric Guitar', 849.99, 36613);
@@ -20,7 +15,4 @@ INSERT INTO GUITAR VALUES (3724920, 'PRS SE Standard 24 Electric Guitar', 649.99
 INSERT INTO GUITAR VALUES (7329790, 'Taylor 322e V-Class Grand Concert Acoustic-Electric Guitar', 2299.99, 90346);
 INSERT INTO GUITAR VALUES (8184316, 'Martin Standard Series 000-18 Auditorium Acoustic Guitar', 2799.99, 80816);
 INSERT INTO GUITAR VALUES (0146632, 'Mitchell MM100 Mini Double-Cutaway Electric Guitar', 99.99, 96852);
-
--- create sample transaction (15-digit id)
---(sample user buys a Gibson F-25 at the last second of xmas eve of '65)
 INSERT INTO PURCHASE VALUES (480927289170066, 0123456789, 3836594, 1965-12-24 11:59:59);
