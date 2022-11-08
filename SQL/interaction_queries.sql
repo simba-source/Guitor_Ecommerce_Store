37c5218773587dcd8002dd551752902146bc2ebc
mperrott <mperrott@uvm.edu>
2022-11-08 16:36:08 -0500

    Added USER and PURCHASE functionality and data

--- SQL/interaction_queries.sql
+++ SQL/interaction_queries.sql
@@ -1,8 +1,16 @@
 -- select a particular guitar, and display the fields on the page from this list
 SELECT * FROM GUITAR WHERE ID = /* the ID */;
 
--- for sorting guitars
+-- when registering a new user, insert their fields
+INSERT INTO USER VALUES (
+/* their generated 10-digit id */,
+/* their first name */, 
+/* their last name */, 
+/* their created username (only alphanumeric)*/, 
+/* their created password (*/, 
+/* their balance starting at $0.00 */);
 
+-- for sorting guitars
 SELECT * FROM GUITAR
 ORDER BY NAME;
 
@@ -17,6 +25,33 @@ SELECT c.Name
 FROM GUITAR g, COMPANY c
 WHERE g.ID = /* the guitar's ID */
 AND g.Company_ID = c.ID;
+
+-- when a purchase is made, modify the balance then insert the associated fields 
+UPDATE USER
+SET Balance = Balance - (SELECT Price FROM GUITAR WHERE ID = /* the guitar's 7-digit ID */)
+WHERE ID = /* the user's 10-digit ID */;
+
+INSERT INTO PURCHASE VALUES (
+/* a generated 15-digit id */, 
+/* the user's 10-digit id */, 
+/* the user's updated balance (current - price of guitar),
+/* the guitar's 7-digit id */, 
+GETDATE());
+
+-- change an account's username or password
+UPDATE USER
+SET Username = /* the new username */
+WHERE ID = /* the user's 10-digit ID */;
+
+-- change an account's username or password
+UPDATE USER
+SET Username = /* the new password */
+WHERE ID = /* the user's 10-digit ID */;
+
+-- view a user's purchases (could be done in a specific view)
+SELECT p.ID, p.Buy_date, g.Name, g.Price, p.User_balance
+FROM PURCHASE p, GUITAR g 
+WHERE p.User_ID = /* the user's id */;
 
 -- add a product as a vendor
 INSERT INTO GUITAR VALUES (
