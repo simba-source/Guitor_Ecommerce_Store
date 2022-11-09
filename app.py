4e4fe2adf17e6d70ad21de24cac9f2be83c012d0
aidan <aidanb363@gmail.com>
2022-11-09 15:20:20 -0500

    added some db functionality

--- app.py
+++ app.py
@@ -54,10 +54,30 @@ def purchase():
 @app.route('/query', methods=['GET', 'POST'])
 def query():
     cur = mysql.connect.cursor()
-    cur.execute("DROP TABLE guitar")
-    cur.execute("CREATE TABLE guitar(ID INT, Name VARCHAR(150), Price DECIMAL(7, 2))")
-    cur.execute("INSERT INTO guitar VALUES (123, 'Fender Strat', 499.99)")
-    cur.execute("SELECT Name FROM guitar")
+    cur.execute("DROP TABLE GUITAR")
+    cur.execute("DROP TABLE COMPANY")
+    cur.execute("DROP TABLE USER")
+    cur.execute("DROP TABLE PURCHASE")
+    cur.execute("CREATE TABLE COMPANY(ID INT, Name VARCHAR(150), PRIMARY KEY (ID))")
+    cur.execute(
+        "CREATE TABLE GUITAR(ID INT, Name VARCHAR(150), Price DECIMAL(7, 2), companyID INT, PRIMARY KEY (ID),FOREIGN KEY (companyID) REFERENCES COMPANY(ID))")
+    cur.execute(
+        "CREATE TABLE USER(ID INT, FName VARCHAR(150), LName VARCHAR(150), Username VARCHAR(150), Password VARCHAR(150), Balance DECIMAL(7,2), PRIMARY KEY (ID))")
+    cur.execute("CREATE TABLE PURCHASE(ID INT, User_ID INT, User_balance DECIMAL(7,2), Item_ID INT, PRIMARY KEY (ID))")
+
+    fd = open('SQL/start_data.sql', 'r')
+    sqlFile = fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
+    for i in sqlCommands:
+        i = i.strip('\n')
+        try:
+            cur.execute(i)
+            print(i)
+            # print(i)
+        except:
+            print("")
+    cur.execute("SELECT * FROM USER WHERE Balance = 100.00")
     data = cur.fetchall()
     return render_template('querytest.html', random_quote=data)
 
