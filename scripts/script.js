f5ff5e1f27277ebde290da9c5eaa1dc1514d62ab
nikhilsaireddychoppa <nchoppa@uvm.edu>
2022-11-19 16:11:34 -0500

    tried to make work the navbar mobile-not worked

--- scripts/script.js
+++ scripts/script.js
@@ -16,39 +16,3 @@ if (close) {
   });
 }
 
-// Single Page- change to images
-
-const mainImage = document.getElementById("MainImage");
-const smallImage = document.getElementsByClassName("small-img");
-
-function click1() {
-  mainImage.src = smallImage[0].src;
-  smallImage[0].classList.add("focus");
-  smallImage[1].classList.remove("focus");
-  smallImage[2].classList.remove("focus");
-  smallImage[3].classList.remove("focus");
-}
-
-function click2() {
-  mainImage.src = smallImage[1].src;
-  smallImage[1].classList.add("focus");
-  smallImage[0].classList.remove("focus");
-  smallImage[2].classList.remove("focus");
-  smallImage[3].classList.remove("focus");
-}
-
-function click3() {
-  mainImage.src = smallImage[2].src;
-  smallImage[2].classList.add("focus");
-  smallImage[1].classList.remove("focus");
-  smallImage[0].classList.remove("focus");
-  smallImage[3].classList.remove("focus");
-}
-
-function click4() {
-  mainImage.src = smallImage[3].src;
-  smallImage[3].classList.add("focus");
-  smallImage[1].classList.remove("focus");
-  smallImage[2].classList.remove("focus");
-  smallImage[0].classList.remove("focus");
-}
