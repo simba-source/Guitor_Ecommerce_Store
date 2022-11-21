8b8d5e0ac6bb33cd17ffa5ec27ca696dea2f39c0
christianrhodes18 <thechristianrhodes@gmail.com>
2022-11-20 19:27:32 -0500

    working on mobile nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
+const mobileNav = document.getElementById("mobile-nav")
 
 // Mobil Navbar Menu open-close
 
 if (bar) {
   bar.addEventListener("click", () => {
-    nav.classList.add("active");
-  });
+    showMobileNavItems()
+  })
 }
 
 if (close) {
@@ -16,3 +17,11 @@ if (close) {
   });
 }
 
+function showMobileNavItems() {
+  console.log("hello!");
+  nav.classList.add("active");
+  const homeLink = document.createElement('a');
+  homeLink.href = "../templates/index.html";
+  homeLink.text = "Home";
+  mobileNav.appendChild(homeLink);
+}
