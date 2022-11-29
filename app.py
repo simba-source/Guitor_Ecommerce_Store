430762988a22bf444078f3e0cfa609fbbeaa0bd9
nikhilsaireddychoppa <nchoppa@uvm.edu>
2022-11-29 11:36:49 -0500

    reformating the app.py file

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ app.config['MYSQL_DATABASE_DB'] = 'mktdata'
 mysql = MySQL(app)
 mysql.init_app(app)
 
-rand_list = []   #for id generation
+rand_list = []  # for id generation
+
 
 # User class to store information about active user
 class User:
@@ -23,7 +24,8 @@ class User:
 # create global instance of class for client
 active_user = User(False, None)
 
-#initialize data in DB
+
+# initialize data in DB
 def data():
     cur = mysql.get_db().cursor()
     cur.execute("DROP TABLE CART_ITEM")
@@ -33,15 +35,20 @@ def data():
     cur.execute("DROP TABLE USER")
     cur.execute("CREATE TABLE COMPANY(ID INT, Name VARCHAR(150), PRIMARY KEY (ID))")
     cur.execute(
-        "CREATE TABLE GUITAR(ID INT, Name VARCHAR(150), Picture VARCHAR(150), Description VARCHAR(150), Price DECIMAL(7, 2), companyID INT, PRIMARY KEY (ID),FOREIGN KEY (companyID) REFERENCES COMPANY(ID))")
+        "CREATE TABLE GUITAR(ID INT, Name VARCHAR(150), Picture VARCHAR(150), Description VARCHAR(150), "
+        "Price DECIMAL(7, 2), companyID INT, PRIMARY KEY (ID),FOREIGN KEY (companyID) REFERENCES COMPANY(ID))")
     cur.execute(
-        "CREATE TABLE USER(ID INT, FName VARCHAR(150), LName VARCHAR(150), Username VARCHAR(150), Password VARCHAR(150), Balance DECIMAL(7,2), PRIMARY KEY (ID))")
+        "CREATE TABLE USER(ID INT, FName VARCHAR(150), LName VARCHAR(150), Username VARCHAR(150), Password VARCHAR("
+        "150), Balance DECIMAL(7,2), PRIMARY KEY (ID))")
     cur.execute(
-        "CREATE TABLE CART(ID INT NOT NULL, User_ID INT, Date_made DATE, PRIMARY KEY (ID), FOREIGN KEY (User_ID) REFERENCES USER(ID))")
+        "CREATE TABLE CART(ID INT NOT NULL, User_ID INT, Date_made DATE, PRIMARY KEY (ID), FOREIGN KEY (User_ID) "
+        "REFERENCES USER(ID))")
     cur.execute(
-        "CREATE TABLE CART_ITEM(ID INT NOT NULL, Item_ID INT, Quantity INT, Date_made DATE, Cart_ID INT, PRIMARY KEY (ID), FOREIGN KEY (Item_ID) REFERENCES GUITAR(ID), FOREIGN KEY (Cart_ID) REFERENCES CART(ID))")
+        "CREATE TABLE CART_ITEM(ID INT NOT NULL, Item_ID INT, Quantity INT, Date_made DATE, Cart_ID INT, PRIMARY KEY "
+        "(ID), FOREIGN KEY (Item_ID) REFERENCES GUITAR(ID), FOREIGN KEY (Cart_ID) REFERENCES CART(ID))")
 
-    # cur.execute("CREATE TABLE PURCHASE(ID INT, User_ID INT, User_balance DECIMAL(7,2), Item_ID INT, PRIMARY KEY (ID))")
+    # cur.execute("CREATE TABLE PURCHASE(ID INT, User_ID INT, User_balance DECIMAL(7,2), Item_ID INT, PRIMARY KEY (
+    # ID))")
 
     fd = open('SQL/start_data.sql', 'r')
     sqlFile = fd.read()
@@ -58,13 +65,15 @@ def data():
     mysql.get_db().commit()
     cur.close()
 
-#login page
+
+# login page
 @app.route('/login', methods=['GET', 'POST'])
 @app.route('/templates/login.html', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-#check login function
+
+# check login function
 @app.route('/checklogin', methods=['GET', 'POST'])
 def checklogin():
     # query for username and password from USERS
@@ -101,15 +110,18 @@ def checklogin():
 
     return render_template('login.html')
 
-#register page
+
+# register page
 @app.route('/register', methods=['GET', 'POST'])
 @app.route('/templates/register.html', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
 
-#register user
+
+# register user
 @app.route('/registeruser', methods=['GET', 'POST'])
 def registeruser():
+    global final_id
     if request.method == "POST":
         # getting input from name tag HTML form
         username = request.form.get("name")
@@ -158,7 +170,8 @@ def registeruser():
     # if it does exist
     return render_template('index.html')
 
-#origin page
+
+# origin page
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/templates/index.html', methods=['GET', 'POST'])
 def origin():
@@ -190,7 +203,8 @@ def shop():
 
     return render_template('shop.html', products=products_dictionary)
 
-#search for product
+
+# search for product
 @app.route('/search', methods=['GET', 'POST'])
 def search_product():
     if request.method == "POST":
@@ -205,7 +219,7 @@ def search_product():
             cur.execute("SELECT Name FROM GUITAR")
             product_names = cur.fetchall()
 
-            #save all product names to a list
+            # save all product names to a list
             guitar_names_list = []
             for name in product_names:
                 guitar_names_list.append(name[0])
@@ -213,7 +227,7 @@ def search_product():
             # use python find method to find all guitar names with substring of search_query
             guitar_names_list_found = []
             for name in guitar_names_list:
-                if name.find(search_query) != -1: #find() returns -1 if value is not found
+                if name.find(search_query) != -1:  # find() returns -1 if value is not found
                     guitar_names_list_found.append(name)
 
             # nested dictionary. Outer for each product, inner for products' keys and values
@@ -223,7 +237,8 @@ def search_product():
             if not guitar_names_list_found:
                 # no guitars match search
                 empty_result = "No results found for your search. Please try again or browse all products. "
-                return render_template('shop.html', products=products_dictionary, active_search=search_query, no_result=empty_result)
+                return render_template('shop.html', products=products_dictionary, active_search=search_query,
+                                       no_result=empty_result)
             else:
                 # search found at least one guitar - display relevant product(s)
                 for guitar_name in guitar_names_list_found:
@@ -244,7 +259,8 @@ def search_product():
 
     return render_template('index.html')
 
-#load product page
+
+# load product page
 @app.route('/loadproduct', methods=['GET', 'POST'])
 def load_product_page():
     product_id = request.args.get('id')
@@ -258,13 +274,15 @@ def load_product_page():
 
     return render_template('product.html', product=product_dictionary)
 
-#product page
+
+# product page
 @app.route('/product', methods=['GET', 'POST'])
 @app.route('/templates/product.html', methods=['GET', 'POST'])
 def product():
     return render_template('product.html')
 
-#cart functionality
+
+# cart functionality
 @app.route('/cart', methods=['GET', 'POST'])
 @app.route('/templates/cart.html', methods=['GET', 'POST'])
 def cart():
@@ -280,7 +298,7 @@ def cart():
 
     # query for all the items in cart
     cur.execute("SELECT Item_ID, CART_ITEM.ID FROM CART_ITEM JOIN CART ON CART.ID = CART_ID WHERE CART.User_ID = %s",
-                (active_user.id))
+                active_user.id)
 
     items = cur.fetchall()
 
@@ -310,7 +328,8 @@ def cart():
 
     return render_template('cart.html', items=cart_items_dictionary, subtotal=total_price)
 
-#add item to cart
+
+# add item to cart
 @app.route('/addtocart', methods=['GET', 'POST'])
 def add_to_cart():
     product_id = request.args.get('product_id')
@@ -333,7 +352,7 @@ def add_to_cart():
         final_id = int(i[-1])
 
     # check if user has cart
-    cur.execute('SELECT * FROM CART WHERE User_ID = (%s)', (active_user.id))
+    cur.execute('SELECT * FROM CART WHERE User_ID = (%s)', active_user.id)
     if not cur.fetchone():
         # user does not have cart. create cart for user
         cur.execute('SELECT * FROM CART WHERE ID = (%s)', final_id)
@@ -351,7 +370,7 @@ def add_to_cart():
             rand_list.append(final_id2)
 
         # check to make sure it isn't in table
-        cur.execute('SELECT * FROM CART_ITEM WHERE ID = (%s)', (final_id2))
+        cur.execute('SELECT * FROM CART_ITEM WHERE ID = (%s)', final_id2)
         if not cur.fetchone():
             cur.execute("INSERT INTO CART_ITEM (ID, Item_ID, Quantity, Cart_ID) VALUES (%s, %s, %s, %s)",
                         (final_id2, product_id, 1, active_user.id))
@@ -363,7 +382,8 @@ def add_to_cart():
     added_to_cart_message = "Item has been added to cart. "
     return render_template('index.html', message=added_to_cart_message)
 
-#remove item from cart function
+
+# remove item from cart function
 @app.route('/removefromcart', methods=['GET', 'POST'])
 def remove_from_cart():
     # get product id from url param
@@ -373,31 +393,34 @@ def remove_from_cart():
     cur = mysql.get_db().cursor()
 
     # remove cart_item from user's cart
-    cur.execute('DELETE FROM CART_ITEM WHERE CART_ITEM.ID = (%s)', (cart_item_id))
+    cur.execute('DELETE FROM CART_ITEM WHERE CART_ITEM.ID = (%s)', cart_item_id)
 
     # commit change to database
     mysql.get_db().commit()
 
     removed_successfully_message = "Item has been removed from cart. "
-    #print(removed_successfully_message)
+    # print(removed_successfully_message)
 
-    #because this is a POST, we want to use redirect
+    # because this is a POST, we want to use redirect
     return redirect('cart')
-    #return render_template('cart.html', message=removed_successfully_message)
+    # return render_template('cart.html', message=removed_successfully_message)
 
-#about page
+
+# about page
 @app.route('/templates/about.html', methods=['GET', 'POST'])
 @app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
 
-#purchase page
+
+# purchase page
 @app.route('/templates/order_placed.html', methods=['GET', 'POST'])
 def final_purchase():
     return render_template('order_placed.html')
 
-#purchased page
-#@app.route('/templates/purchased.html', methods=['GET', 'POST'])
+
+# purchased page
+# @app.route('/templates/purchased.html', methods=['GET', 'POST'])
 @app.route('/purchased', methods=['GET', 'POST'])
 def purchased():
     price = request.args.get('price')
@@ -407,7 +430,7 @@ def purchased():
 
     # query to check if any items are in user's cart before rendering purchased page
     cur.execute("SELECT Item_ID, CART_ITEM.ID FROM CART_ITEM JOIN CART ON CART.ID = CART_ID WHERE CART.User_ID = %s",
-                (active_user.id))
+                active_user.id)
 
     has_cart_item = cur.fetchone()
 
@@ -420,7 +443,8 @@ def purchased():
         error_message = "No Items in Cart"
         return render_template('cart.html', message=error_message)
 
-#for testing
+
+# for testing
 @app.route('/query', methods=['GET', 'POST'])
 def query():
     cur = mysql.get_db().cursor()
